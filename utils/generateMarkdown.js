c2e290cef11d82ef4881e0682dc0960d0d6777a6
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-15 11:52:58 -0500

    Begin generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,7 +15,51 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+// license badge
+
+${data.blurb}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contribute](#contribute)
+- [Contact Me](#questions)
+- [License](#license)
+## Description
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Tests
+
+${data.tests}
+
+## Contribute
+
+${data.contribute}
+
+## Questions?
+
+- Github: [${data.github}]
+- LinkedIn: [${data.author}]
+- Email: [${data.email}]
+
+## License
+
+// license section
+
+&copy; ${data.year} ${data.author}
 `;
-}
+};
 
 module.exports = generateMarkdown;
