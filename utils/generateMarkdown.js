856f2fb6377db18b4de33b7802783d88f52d1bb6
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-04-15 12:07:59 -0500

    Define renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -38,7 +38,20 @@ function renderLicenseLink(license) {
 
 // Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { };
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return `This project is licensed under the MIT License.
+  For more information, please visit: [MIT](${renderLicenseLink(license)})`;
+  } else if (license === "Apache 2.0") {
+    return `This project is licensed under Apache 2.0.
+  For more information, please visit: [Apache](${renderLicenseLink(license)})`;
+  } else if (license === "GPL 3.0") {
+    return `This project is licensed under GPL 3.0.
+  For more information, please visit: [GPL](${renderLicenseLink(license)})`;
+  } else {
+    return '';
+  }
+};
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
